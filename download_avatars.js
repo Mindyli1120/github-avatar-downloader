15fd076ff22f1b7621d3243025fdb9e23cebc930
mindyli1120 <mindyli1989@gmail.com>
2018-06-12 02:18:26 +0000

    update of download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var github_token = require('./secrets');
-
+var fs = require('fs');
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -9,16 +9,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
             'Authorization': 'token ' + github_token.GITHUB_TOKEN
         }
     };
-    request(options, function(err, res, body) {
+    request(options, function (err, res, body) {
         var jsonBody = JSON.parse(body);
-        cb(err, jsonBody) 
+        cb(err, jsonBody)
     });
 }
 
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+        .on('error', function (err) {
+            throw err;
+        })
+        .pipe(fs.createWriteStream(filePath));
+}
 
 getRepoContributors("jquery", "jquery", function (err, result) {
     console.log("Errors:", err);
     for (var i = 0; i < result.length; i++) {
-        console.log("avatar_url: " + result[i].avatar_url);    
+        var login = result[i].login;
+        //get the image of each person with the avatar_url
+        downloadImageByURL(result[i].avatar_url, "./avatars/" + login + ".jpg")
     }
-});  
+});
+
