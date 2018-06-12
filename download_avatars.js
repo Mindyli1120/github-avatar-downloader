b843519a576f0a34e89462773e2e7a8da742c736
mindyli1120 <mindyli1989@gmail.com>
2018-06-12 01:32:50 +0000

    Got the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,24 @@
 var request = require('request');
-var github_token = require('1a370cf72a2e15b8dbc9b464ddcd29b83cba7bf0');
+var github_token = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'request',
+            'Authorization': 'token ' + github_token.GITHUB_TOKEN
         }
     };
     request(options, function(err, res, body) {
-        cb(err, body);
+        var jsonBody = JSON.parse(body);
+        cb(err, jsonBody) 
     });
 }
 
 
 getRepoContributors("jquery", "jquery", function (err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    for (var i = 0; i < result.length; i++) {
+        console.log("avatar_url: " + result[i].avatar_url);    
+    }
 });  
